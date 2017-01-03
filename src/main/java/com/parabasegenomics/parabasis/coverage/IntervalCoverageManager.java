54a54b3dacab0b92eb0c115ab9d862e462902f65
evanmauceli <emauceli@parabasegenomics.com>
2017-01-03 15:05:14 -0500

    working towards a good refactor of the IntervalCoverage class and manipulators.

---
+++ src/main/java/com/parabasegenomics/parabasis/coverage/IntervalCoverageManager.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.parabasegenomics.parabasis.coverage;
+
+import htsjdk.samtools.SAMFileHeader;
+import htsjdk.samtools.SAMRecord;
+import htsjdk.samtools.SAMRecordIterator;
+import htsjdk.samtools.SamReader;
+import htsjdk.samtools.SamReaderFactory;
+import htsjdk.samtools.ValidationStringency;
+import htsjdk.samtools.util.Interval;
+import htsjdk.samtools.util.OverlapDetector;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class to manage a list of IntervalCoverage objects.  Used to manage coverage
+ * of many intervals making up an assay (or test). 
+ * @author evanmauceli
+ */
+public class IntervalCoverageManager {
+    
+    private final String assayName;
+    private final List<IntervalCoverage> intervalCoverages;
+    private final Map<String,Integer> modelIntervalIndexMap;
+    private final SamReaderFactory samReaderFactory ;
+    private final OverlapDetector targetOverlapDetector;
+    
+    private SAMFileHeader samFileHeader;     
+    private SamReader samReader;
+    
+    
+    public IntervalCoverageManager(String assay, List<Interval> intervals) {
+        assayName=assay;
+        intervalCoverages=new ArrayList<>();
+        modelIntervalIndexMap = new HashMap<>();
+        samReaderFactory 
+            = SamReaderFactory.makeDefault()
+                .validationStringency(ValidationStringency.SILENT);
+        
+        targetOverlapDetector = new OverlapDetector<>(0,0);
+        targetOverlapDetector.addAll(intervals,intervals);
+        
+        // create a new IntervalCoverage object for each target in the assay
+        // set the mapping of interval to index in list
+        Integer index=0;
+        for (Interval interval : intervals) {
+            intervalCoverages.add(new IntervalCoverage(interval));
+            modelIntervalIndexMap.put(stringifyInterval(interval), index);
+            index++;
+        }
+        
+    }
+    
+    
+    /**
+     * Read through a bam file record by record, updating the appropriate IntervalCoverage 
+     * objects as we go.  This method ignores the following reads:
+     *      Fails vendor quality 
+     *      Not primary alignment
+     *      Unmapped read
+     *      Duplicate read
+     * @param bamFile Bam file to parse.
+     * @throws java.io.IOException
+     */
+    public void parseBam(File bamFile) 
+    throws IOException {
+        samReader = samReaderFactory.open(bamFile);
+
+        SAMRecordIterator samRecordIterator = samReader.iterator();
+        SAMRecord samRecord;
+        while (samRecordIterator.hasNext()) {
+            samRecord = samRecordIterator.next();
+            if (samRecord.getReadFailsVendorQualityCheckFlag()) {
+                continue;
+            }
+            if (samRecord.getNotPrimaryAlignmentFlag()) {
+                continue;
+            } 
+            if (samRecord.getReadUnmappedFlag()) {
+                continue;
+            }
+            if (samRecord.getDuplicateReadFlag()) {
+                continue;
+            }
+
+            Interval recordInterval 
+                = new Interval(
+                    samRecord.getReferenceName(),
+                    samRecord.getAlignmentStart(),
+                    samRecord.getAlignmentEnd());
+
+            Collection<Interval> overlappingTargets 
+                = targetOverlapDetector.getOverlaps(recordInterval);
+
+            for (Interval overlappingTarget : overlappingTargets) {
+                String overlappingTargetAsString 
+                    = stringifyInterval(overlappingTarget);
+                if (!modelIntervalIndexMap
+                    .containsKey(overlappingTargetAsString)) {
+                    throw new IllegalArgumentException(
+                        "Could not find interval in model: "
+                        + overlappingTargetAsString);
+                }
+                int indexIntoIntervalCoverages 
+                    = modelIntervalIndexMap.get(stringifyInterval(overlappingTarget));
+
+                final IntervalCoverage intervalCoverage 
+                    = intervalCoverages.get(indexIntoIntervalCoverages);
+                for (int position = samRecord.getAlignmentStart(); 
+                         position <= samRecord.getAlignmentEnd();
+                        ++position) {
+                    // the -1 is to convert from htsjdk intervals to ours
+                    intervalCoverage.incrementCoverageCount(position-1);
+                }
+            }              
+        }        
+        samReader.close();
+    }
+    
+    
+    /**
+     * Produce a String from the given Interval object.  The String needs to be
+     * unique avoid collisions when searching for held Intervals.
+     * @param interval
+     * @return Returns a unique (for this set of Intervals) String identifying
+     * this Interval.
+     */
+    private String stringifyInterval(Interval interval) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(interval.getContig());
+        builder.append(":");
+        builder.append(interval.getStart());
+        builder.append("-");
+        builder.append(interval.getEnd());
+        return (builder.toString());
+    }
+    
+}
